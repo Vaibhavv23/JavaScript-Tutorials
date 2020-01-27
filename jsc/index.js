68f444a2addcc1622195b4faa042c35ab1aa520b
Vaibhavv23 <iamvaibhav2000@gmail.com>
2020-01-27 15:25:58 +0530

    first commit

---
+++ jsc/index.js
@@ -0,0 +1,55 @@
+/************
+ * Data types>>>>
+ * Number
+ * String
+ * Boolean
+ * Undefined
+ * Null
+ */
+
+/********************
+ * Simple console log and variable name tutorial.
+ */
+
+var firstName = 'Vaibhav';
+console.log(firstName);
+
+var lastName = 'Vishal';
+console.log(lastName);
+
+/***************
+ * Type coersion
+ */
+
+firstName = 'Ram'; // string
+var age = 34; // number or undefined
+console.log(firstName +' '+ age); // string
+
+/***************
+ * Variable mutation
+ */
+
+var job, ismarried;
+job = 'teacher';
+ismarried = false;
+alert(firstName +' is a '+ age + ' year old '+ job + '. Is he married ?' + ismarried);
+
+lastName = prompt('What is your lastname?');
+console.log(firstName +' '+ lastName);
+//On cancelling prompt log=null.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
