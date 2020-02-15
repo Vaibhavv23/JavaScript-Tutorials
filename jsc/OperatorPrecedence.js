d7d75e99c27aa8326eccc59b08dddfb0233301f9
Vaibhavv23 <iamvaibhav2000@gmail.com>
2020-02-15 21:21:42 +0530

    added code

--- jsc/OperatorPrecedence.js
+++ jsc/OperatorPrecedence.js
@@ -1,22 +1,22 @@
 /****************
  * Operator Precedence
- */
+*/
 
- var now = 2020;
- var yearVaibhav = 2002;
- var fullAge = 20;
+var now = 2020;
+var yearVaibhav = 2002;
+var fullAge = 20;
 
- // Multiple operators
- var isFullAge = now - yearVaibhav >= fullAge; // follow operator precedence rule.
- console.log(isFullAge);
+// Multiple operators
+var isFullAge = now - yearVaibhav >= fullAge; // follow operator precedence rule.
+console.log(isFullAge);
 
- // Grouping
- var ageVaibhav = now - yearVaibhav;
- var ageRam = 26;
- var average = (ageVaibhav + ageRam) / 2;
- console.log(average);
+// Grouping
+var ageVaibhav = now - yearVaibhav;
+var ageRam = 26;
+var average = (ageVaibhav + ageRam) / 2;
+console.log(average);
 
- // Multiple assignments
+// Multiple assignments
 var x, y;
 x = y = (3 + 5) * 4 - 6; // Assignment operator follow right to left rule.
 console.log(x, y); // 26 26
